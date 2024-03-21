dcc0463fbb1a2dc48b6389bb47dd5e815bd3b9b6
singodiyashubham87 <singodiyashubham87@gmail.com>
2024-03-21 13:06:25 +0530

    v1.0.1: add Loader component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-export default function test() {
-  console.log("Hello World");
+import "./index.css";
+
+function Loader({ logo }) {
+  return (
+    <div className="loader h-[100vh] w-[100vw] flex justify-center items-center bg-black">
+      <div className="loading h-[100%] w-[100%] flex flex-col sm:flex-row justify-center items-center border-4 border-white-800">
+        <img
+          id="logo"
+          src={logo}
+          alt="loader"
+          className="border-4  border-white w-[150px] sm:w-[200px] rounded-[50%] sm:mr-[1rem] sm:mb-[0] mb-[2rem] animate-spin"
+        />
+        <h5 className="textt text-white  text-[1rem] vvsm:text-[1.5rem] vsm:text-[1.7rem] md:text-[2.5rem] sm:text-[2rem] font-primary">
+          Loading...
+        </h5>
+      </div>
+    </div>
+  );
 }
+
+export default Loader;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'custom': '5px 10px #565637',
+      },
+      fontFamily: {
+        primary: 'Quicksand',
+      },
+      screens: {
+        '4xl': '1801px',
+        '3xl': '1751px',
+        'mmd': '901px',
+        'msm': '631px',
+        'gsm': '531px',
+        'buttonsm': '441px',
+        'vsm': '406px',
+        'formsm': '351px',
+        'vvsm': '100px',
+      },
+      animation: {
+        'spin': 'spin 2s linear infinite',
+      }
+    }
+  },
+  plugins: [],
+}
